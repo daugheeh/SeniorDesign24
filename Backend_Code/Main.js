e6d0469f7dcf915bdff3c877027399dcc31d0009
Donald Gross <dgross64681@gmail.com>
2024-03-20 16:02:56 -0400

    Tinkering with images

--- Backend_Code/Main.js
+++ Backend_Code/Main.js
@@ -12,5 +12,27 @@ async function main() {
   }
 }
 
-main();
+function generateRandomNumbers(min, max, count) {
+  let randomNumbers = [];
+
+  for (let i = 0; i < count; i++) {
+    let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    randomNumbers.push(randomNumber);
+  }
+
+  return randomNumbers;
+}
+
+main()
+{
+  const min = 1;
+  const max = 612;
+  const count = 9;
+
+  const randomNumbers = generateRandomNumbers(min, max, count);
+  do{
+    
+  }while(randomNumbers != null)
+
+};
 
