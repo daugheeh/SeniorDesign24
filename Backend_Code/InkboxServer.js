05a00726c93c247b0476e8a8a02080d4f81e4689
Donald Gross <dgross64681@gmail.com>
2024-03-26 15:30:02 -0400

    Added more changes to backend calls

--- Backend_Code/InkboxServer.js
+++ Backend_Code/InkboxServer.js
@@ -1,3 +1,7 @@
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://daugherty704:8Ac9nAVcO0y0kWo3@inkbase.mc8fboc.mongodb.net/";
+const client = new MongoClient(uri);
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,16 +12,26 @@ app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
 
+async function fetchDataFromMongoDB(query){
+  await client.connect();
+  const dbInbase = client.db("InbaseData");
+  const InbaseData = dbInbase.collection("Data");
+  const cardName = {"NAME": query};
+  const result = InbaseData.findOne(cardName);
+  return result;
+}
+
 // Example of a GET endpoint to fetch data
 app.get('/api/data', async (req, res) => {
-    const data = await fetchDataFromMongoDB();
+    const data = await fetchDataFromMongoDB(req.query.cardName);
+    //const data = await fetchDataFromMongoDB("Goofy - Musketeer");
     res.json(data);
   });
   
   // Example of a POST endpoint to store data
-  app.post('/api/users', async (req, res) => {
+app.post('/api/users', async (req, res) => {
     const userData = req.body;
     await saveDataToMongoDB(userData);
     res.status(201).send('User data stored');
-  });
+});
   
--- Backend_Code/Main.js
+++ Backend_Code/Main.js
@@ -1,17 +1,3 @@
-import { MongoClient } from 'mongodb';
-const uri = "mongodb+srv://daugherty704:8Ac9nAVcO0y0kWo3@inkbase.mc8fboc.mongodb.net/";
-const client = new MongoClient(uri);
-
-async function main() {
-  try {
-    await client.connect();
-    console.log("Connected to MongoDB");
-    // You can now interact with your MongoDB database here
-  } catch (e) {
-    console.error(e);
-  }
-}
-
 function generateRandomNumbers(min, max, count) {
   let randomNumbers = [];
 
