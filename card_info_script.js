56b30eacaf66b413da17d2333f8ce8f4d0de7479
D 1 26 26 <38295166+dazzdingo@users.noreply.github.com>
2024-04-08 09:56:18 -0400

    update in card-info page

---
+++ card_info_script.js
@@ -0,0 +1,113 @@
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://daugherty704:8Ac9nAVcO0y0kWo3@inkbase.mc8fboc.mongodb.net/";
+const client = new MongoClient(uri);
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json()); // Middleware to parse JSON bodies
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
+
+async function fetchDataFromMongoDB(query) {
+  await client.connect();
+  let dbInbase = client.db("InbaseData");
+  let InbaseData = dbInbase.collection("NewData2");
+  filters = [];
+  let baseQuery = "";
+  if (false) {
+    let queryNameObject = [];
+    if (!queryNameObject["$and"]) {
+      queryNameObject["$and"] = [];
+    }
+    queryNameObject["$and"].push({ "NAME": new RegExp(query.cardText) });
+
+    const sortSearch = query.name;
+    const sortSearchCriteria = { [sortSearch]: 1 };
+    let searchResult = await InbaseData.find(queryNameObject).sort(sortSearchCriteria).toArray();
+    return searchResult;
+  }
+  else {
+    if (query.rarity != "None") {
+      filters.push(`{"RARITY": "${query.rarity}"}`);
+    }
+    if (query.set != "None") {
+      filters.push(`{"SET": "${query.set}"}`);
+    }
+    if (query.inkCost != "") {
+      filters.push(`{"COST": ${query.inkCost}}`);
+    }
+
+    if (Array.isArray(query.colors)) {
+      let multiColor = '{"$or": [';
+      query.colors.forEach((element, index) => {
+        multiColor += `{"INK": "${element}"}`;
+        if (index != query.colors.length - 1) {
+          multiColor += ", ";
+        }
+      })
+      multiColor += "]}"
+      filters.push(multiColor);
+    }
+    else if (typeof query.colors == 'string') {
+
+      if (query.colors != "") {
+        filters.push(`{"INK": "${query.colors}"}`);
+      }
+    }
+  }
+
+  baseQuery += '{"$and": [';
+  filters.forEach((element, index) => {
+    baseQuery += element;
+    if (index != filters.length - 1) {
+      baseQuery += ", "
+    }
+  });
+  baseQuery += "]}";
+  let queryObject = JSON.parse(baseQuery);
+  if (query.cardText !== "") {
+    if (!queryObject["$and"]) {
+      queryObject["$and"] = [];
+    }
+    queryObject["$and"].push({ "ABILITY": new RegExp(query.cardText) });
+  }
+  console.log(queryObject);
+  const sortField = query.sortBy;
+  const sort = { [sortField]: 1 };
+  let result = InbaseData.find(queryObject);
+  result = await result.sort(sort).toArray();
+  return result;
+}
+
+app.get('/api/data', async (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  answer = await fetchDataFromMongoDB(req.query);
+  console.log(answer);
+  res.json(answer);
+  return res;
+});
+
+
+async function fetchAndDisplayCard() {
+  try {
+      const response = await fetch('/api/data'); 
+      const card = await response.json(); 
+      document.getElementById('cardName').textContent = card.NAME;
+      document.getElementById('cardImage').src = card['IMAGE LINK']; 
+      document.getElementById('cardImage').alt = card.NAME;
+      document.getElementById('cardClassifications').textContent = `Classifications: ${card.CLASSIFICATIONS}`;
+      document.getElementById('cardAttributes').innerHTML = `Strength: ${card.STR} <br> Willpower: ${card.WILL} <br> Lore: ${card.LORE}`;
+      document.getElementById('cardLore').innerHTML = `Set: ${card.SET} <br> Number: ${card.CARDNUM} <br> Rarity: ${card.RARITY}`;
+      document.getElementById('cardText').innerHTML = `Card Text: <br> ${card.ABILITY}`;
+
+  } catch (error) {
+      console.error('Error fetching card data:', error);
+  }
+};
+
+// Call the function when the page loads
+fetchAndDisplayCard()
