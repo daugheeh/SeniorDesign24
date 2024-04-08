66f7dc70f369e1b503c1861fa6e37372c7baaf42
Donald Gross <dgross64681@gmail.com>
2024-04-08 13:03:55 -0400

    change

--- collection_script.js
+++ collection_script.js
@@ -33,7 +33,6 @@ document.addEventListener('DOMContentLoaded', function() {
         window.location.href = 'filter.html';
     });
 });
-
 const { MongoClient } = require('mongodb');
 const uri = "mongodb+srv://daugherty704:8Ac9nAVcO0y0kWo3@inkbase.mc8fboc.mongodb.net/";
 const client = new MongoClient(uri);
